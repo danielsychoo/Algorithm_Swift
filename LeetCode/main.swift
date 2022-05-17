1c4866ea0c4ac56dd9e16d43076cb17b79792deb
danielsychoo <sungyeop.choo@gmail.com>
2022-05-17 12:27:26 +0900

    [Update] Add bestPractice on 3.Longest Substring Without Repeating Characters

--- LeetCode/main.swift
+++ LeetCode/main.swift
@@ -39,22 +39,66 @@ let output = 3
 // let output = 1
 
 // example 3
+// var s = "dvdf"
+// let output = 3
+
+// example 4
+// var s = "au"
+// let output = 2
+
+// example 5
 // var s = "pwwkew"
 // let output = 3
+
+// edge 1
+// var s = " "
+// let output = 1
+
+// edge 2
+// var s = ""
+// let output = 0
 
 
 // MARK: - My Solution
 
-func lengthOfLongestSubstring(_ s: String) -> Int {
-    return 0
-}
+// 못품..
 
 
 // MARK: - Play
 
-// let result = lengthOfLongestSubstring(s) == output
-// print("result :", result)
-
+let result = lengthOfLongestSubstring(s) == output
+print("result :", result)
 
 
 // MARK: - Best practice
+
+// - Complexity:
+//   - time: O(n)
+
+/*
+ - 주석[1]
+ (Array.firstIndex(of element:) -- https://developer.apple.com/documentation/swift/array/2994720-firstindex)
+ (문자열 문자 단위로 다루기 -- http://seorenn.blogspot.com/2018/05/swift-string-index.html)
+ Array.firstIndex를 이용해 chars 내부에서 char의 첫 번째 index를 optionalBinding으로 가져오고,
+    optionalBinding이 통과하면 해당 index+1 부터 끝까지로 Array 재생성해서 chars에 재할당
+    
+ 결과적으로 중복된 char 이후부터의 array가 재생성됨으로서 substring 비교
+ */
+
+func lengthOfLongestSubstring(_ s: String) -> Int {
+    var max = 0
+    var chars: [Character] = []
+    
+    let arr = Array(s)
+    let length = s.count
+    
+    for i in 0..<length {
+        let char = arr[i]
+        
+        if let same_char_index = chars.firstIndex(of: char) { chars = Array(chars[(same_char_index+1)..<chars.count]) } /// 주석[1]
+        chars.append(char)
+        max = chars.count > max ? chars.count : max
+    }
+
+    return max
+}
