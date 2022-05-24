ef71fdcf87107152da0addb574fe1c032a1c0f6b
danielsychoo <sungyeop.choo@gmail.com>
2022-05-24 09:06:27 +0900

    [Update] Change problems to study together

--- LeetCode/main.swift
+++ LeetCode/main.swift
@@ -2,48 +2,57 @@
 //  main.swift
 //  Algorithm_Swift
 //
-//  Created by sungyeopTW on 2022/05/20.
+//  Created by sungyeopTW on 2022/05/24.
 //
 
 
 // MARK: - Description
 
 /*
- 5. Longest Palindromic Substring
+ 136. Single Number
  
- Given a string s, return the longest palindromic substring in s.
+ Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+ You must implement a solution with a linear runtime complexity and use only constant extra space.
  
  Example 1:
- Input: s = "babad"
- Output: "bab"
- Explanation: "aba" is also a valid answer.
+ Input: nums = [2,2,1]
+ Output: 1
  
  Example 2:
- Input: s = "cbbd"
- Output: "bb"
+ Input: nums = [4,1,2,1,2]
+ Output: 4
+ 
+ Example 3:
+ Input: nums = [1]
+ Output: 1
  */
 
 
 // MARK: - Inputs
 
 // example 1
-let s = "babad"
-let output = "bab"
+let nums = [2,2,1]
+let output = 1
 
 // example 2
-// let s = "cbbd"`
-// let output = "bb"
+// let nums = [4,1,2,1,2]
+// let output = 4
+
+// example 3
+// let nums = [1]
+// let output = 1
 
 
 // MARK: - My Solution
 
-func longestPalindrome(_ s: String) -> String {
-    return ""
+func singleNumber(_ nums: [Int]) -> Int {
+    return 0
 }
+
 
 // MARK: - Play
 
-let result = longestPalindrome(s) == output
+let result = singleNumber(nums) == output
 print("result :", result)
 
 
