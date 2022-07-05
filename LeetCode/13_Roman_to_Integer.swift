5772465829b5a982463420b0e23db2457d4afda4
danielsychoo <sungyeop.choo@gmail.com>
2022-07-06 08:49:34 +0900

    [Feat] Add leetCode 12.Integer to Roman

--- LeetCode/13_Roman_to_Integer.swift
+++ LeetCode/13_Roman_to_Integer.swift
@@ -2,13 +2,13 @@
 //  main.swift
 //  Algorithm_Swift
 //
-//  Created by sungyeopTW on 2022/07/06.
+//  Created by sungyeopTW on 2022/07/05.
 //
 
 // MARK: - Description
 
 /*
- 12. Integer to Roman
+ 13. Roman to Integer
  
  Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
 
@@ -20,28 +20,28 @@
  C             100
  D             500
  M             1000
- For example, 2 is written as II in Roman numeral, just two one's added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+ For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
 
  Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
 
  I can be placed before V (5) and X (10) to make 4 and 9.
  X can be placed before L (50) and C (100) to make 40 and 90.
  C can be placed before D (500) and M (1000) to make 400 and 900.
- Given an integer, convert it to a roman numeral.
+ Given a roman numeral, convert it to an integer.
  
  Example 1:
- Input: num = 3
- Output: "III"
- Explanation: 3 is represented as 3 ones.
+ Input: s = "III"
+ Output: 3
+ Explanation: III = 3.
  
  Example 2:
- Input: num = 58
- Output: "LVIII"
- Explanation: L = 50, V = 5, III = 3.
+ Input: s = "LVIII"
+ Output: 58
+ Explanation: L = 50, V= 5, III = 3.
  
  Example 3:
- Input: num = 1994
- Output: "MCMXCIV"
+ Input: s = "MCMXCIV"
+ Output: 1994
  Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
  */
 
@@ -49,16 +49,16 @@
 // MARK: - Inputs
 
 // example 1
-// let num = 3
-// let output = "III"
+// let s = "III"
+// let output = 3
 
 // example 2
-// let num = 58
-// let output = "LVIII"
+// let s = "LVIII"
+// let output = 58
 
 // example 3
-let num = 1994
-let output = "MCMXCIV"
+// let s = "MCMXCIV"
+// let output = 1994
 
 
 // MARK: - My Solution
@@ -69,10 +69,30 @@ let output = "MCMXCIV"
  3. 변환결과를 더함
  */
 
-func intToRoman(_ num: Int) -> String {
-    return ""
-}
-
+// func romanToInt(_ s: String) -> Int {
+//     let arr = Array(s).map { convertToInt($0) }
+//
+//     var result = 0
+//     var i = 1
+//     while arr.count >= i {
+//
+//         if arr.count == i {
+//             result += arr[arr.count-1]
+//             break
+//         }
+//
+//         if arr[i] > arr[i-1] {
+//             result += (arr[i] - arr[i-1])
+//             i += 2
+//         } else {
+//             result += arr[i-1]
+//             i += 1
+//         }
+//     }
+//
+//     return result
+// }
+//
 // func convertToInt(_ symbol: Character) -> Int {
 //     switch symbol {
 //     case "I":
@@ -95,8 +115,10 @@ func intToRoman(_ num: Int) -> String {
 
 // MARK: - Play
 
-let result = intToRoman(num) == output
-print("result :", result)
+// let result = romanToInt(s) == output
+// print("result :", result)
 
 
 // MARK: - Best practice
+
+
--- README.md
+++ README.md
@@ -6,6 +6,7 @@
 - [3. Longest substring without repeating characters](/LeetCode/3_Longest_substring_without_repeating_characters.swift)
 - [4. Median of Two Sorted Arrays](/LeetCode/4_Median_of_Two_Sorted_Arrays.swift)
 - [9. Palindrome Number](/LeetCode/9_Palindrome_Number.swift)
+- [13. Roman to Integer](/LeetCode/13_Roman_to_Integer.swift)
 - [136. Single Number](/LeetCode/136_Single_Number.swift)
 - [946. Validate Stack Sequences](/LeetCode/946_Validate_Stack_Sequences.swift)
 - [1017. Convert to Base -2](/LeetCode/1017_Convert_to_Base_-2.swift)
@@ -14,4 +15,4 @@
 - [1700. Number of Students Unable to Eat Lunch](/LeetCode/1700_Number_of_Students_Unable_to_Eat_Lunch.swift)
 - [1980. Find Unique Binary String](/LeetCode/1980_Find_Unique_Binary_String.swift)
 - [2027. Minimum Moves to Convert String](/LeetCode/2027_Minimum_Moves_to_Convert_String.swift)
-- [13. Roman to Integer](/LeetCode/main.swift)
+- [12. Integer to Roman](/LeetCode/main.swift)
