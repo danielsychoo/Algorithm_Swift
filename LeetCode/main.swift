51e2d33a493c528f67b4e514564f8c2d257fef4f
danielsychoo <sungyeop.choo@gmail.com>
2022-07-05 10:08:40 +0900

    [Update] Solve leetCode 13. Roman to Integer

--- LeetCode/main.swift
+++ LeetCode/main.swift
@@ -49,26 +49,67 @@
 // MARK: - Inputs
 
 // example 1
-let s = "III"
-let output = 3
+// let s = "III"
+// let output = 3
 
 // example 2
 // let s = "LVIII"
 // let output = 58
 
 // example 3
-// let s = "MCMXCIV"
-// let output = 1994
+let s = "MCMXCIV"
+let output = 1994
 
 
 // MARK: - My Solution
 
 /*
-
+ 1. 문자를 쭉 읽으며 앞의 수가 더 크면 그대로
+ 2. 앞의 수가 더 작으면 (-앞의 수 + 뒤의 수)
+ 3. 변환결과를 더함
  */
 
 func romanToInt(_ s: String) -> Int {
-    return 0
+    let arr = Array(s).map { convertToInt($0) }
+    
+    var result = 0
+    var i = 1
+    while arr.count >= i {
+        
+        if arr.count == i {
+            result += arr[arr.count-1]
+            break
+        }
+        
+        if arr[i] > arr[i-1] {
+            result += (arr[i] - arr[i-1])
+            i += 2
+        } else {
+            result += arr[i-1]
+            i += 1
+        }
+    }
+    
+    return result
+}
+
+func convertToInt(_ symbol: Character) -> Int {
+    switch symbol {
+    case "I":
+        return 1
+    case "V":
+        return 5
+    case "X":
+        return 10
+    case "L":
+        return 50
+    case "C":
+        return 100
+    case "D":
+        return 500
+    default:
+        return 1000
+    }
 }
 
 
