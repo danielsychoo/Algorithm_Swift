c326ce1e507553c2229ed525bbc5436a42978f73
danielsychoo <sungyeop.choo@gmail.com>
2022-07-06 10:34:22 +0900

    [Update] Solve leetCode 12. Intiger to Roman

--- LeetCode/main.swift
+++ LeetCode/main.swift
@@ -57,40 +57,90 @@
 // let output = "LVIII"
 
 // example 3
-let num = 1994
-let output = "MCMXCIV"
+// let num = 1994
+// let output = "MCMXCIV"
+
+// example 4
+let num = 20
+let output = "XX"
 
 
 // MARK: - My Solution
 
 /*
- 1. 문자를 쭉 읽으며 앞의 수가 더 크면 그대로
- 2. 앞의 수가 더 작으면 (-앞의 수 + 뒤의 수)
- 3. 변환결과를 더함
+ 1. func intToRoman -- num을 Int배열로 변경
+ 2. func outerCalculate -- num의 자릿수에 따라 연산 분기
+ 3. func innerCalculate -- 각 자릿수에 Symbol 연산
  */
 
 func intToRoman(_ num: Int) -> String {
-    return ""
+    
+    let temp = Array(String(num))
+    let arr = temp.map { Int(String($0))! }
+    
+    return outerCalculate(arr)
 }
 
-// func convertToInt(_ symbol: Character) -> Int {
-//     switch symbol {
-//     case "I":
-//         return 1
-//     case "V":
-//         return 5
-//     case "X":
-//         return 10
-//     case "L":
-//         return 50
-//     case "C":
-//         return 100
-//     case "D":
-//         return 500
-//     default:
-//         return 1000
-//     }
-// }
+func outerCalculate(_ arr: [Int]) -> String {
+    var result = ""
+    
+    switch arr.count { /// 자릿수에 따라 분기
+    case 4:
+        for _ in 1 ... arr[0] {
+            result += "M"
+        }
+        result += innerCalculate(from: (100, arr[1]))
+        result += innerCalculate(from: (10, arr[2]))
+        result += innerCalculate(from: (1, arr[3]))
+    case 3:
+        result += innerCalculate(from: (100, arr[0]))
+        result += innerCalculate(from: (10, arr[1]))
+        result += innerCalculate(from: (1, arr[2]))
+    case 2:
+        result += innerCalculate(from: (10, arr[0]))
+        result += innerCalculate(from: (1, arr[1]))
+    default:
+        result += innerCalculate(from: (1, arr[0]))
+    }
+    
+    return result
+}
+
+func innerCalculate(from value: (Int, Int)) -> String { /// (자리수, 값)
+    if value.1 == 0 { return "" } /// 0이면 연산 ㄴㄴ
+    
+    var symbols: [String] = [] /// 자리수에 따라 사용될 symbol들
+    switch value.0 {
+    case 100:
+        symbols = ["C", "CD", "D", "CM"]
+    case 10:
+        symbols = ["X", "XL", "L", "XC"]
+    default:
+        symbols = ["I", "IV", "V", "IX"]
+    }
+    
+    var result = ""
+    
+    if value.1 < 4 { /// 연산
+        for _ in 1 ... value.1 {
+            result += symbols[0]
+        }
+    } else if value.1 == 4 {
+        result += symbols[1]
+    } else if value.1 == 5 {
+        result += symbols[2]
+    } else if value.1 == 9 {
+        result += symbols[3]
+    } else {
+        result += symbols[2]
+        
+        for _ in 1 ... value.1 - 5 {
+            result += symbols[0]
+        }
+    }
+    
+    return result
+}
 
 
 // MARK: - Play
