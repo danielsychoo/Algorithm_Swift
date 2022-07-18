5453420dffde9c65a7a225302159f37c7aaf7d81
danielsychoo <sungyeop.choo@gmail.com>
2022-07-18 11:19:21 +0900

    [Feat] Add leetCode 1431. Kids With the Greatest Number of Candies

---
+++ LeetCode/1119_Remove_Vowels_from_a_String.swift
@@ -0,0 +1,59 @@
+//
+//  main.swift
+//  Algorithm_Swift
+//
+//  Created by sungyeopTW on 2022/07/18.
+//
+
+// MARK: - Description
+
+/*
+ 1119. Remove Vowels from a String
+ 
+ Given a string s, remove the vowels 'a', 'e', 'i', 'o', and 'u' from it, and return the new string.
+ 
+ Example 1:
+ Input: s = "leetcodeisacommunityforcoders"
+ Output: "ltcdscmmntyfrcdrs"
+ 
+ Example 2:
+ Input: s = "aeiou"
+ Output: ""
+ */
+
+
+// MARK: - Inputs
+
+// example 1
+// let s = "leetcodeisacommunityforcoders"
+// let output = "ltcdscmmntyfrcdrs"
+
+// example 2
+// let s = "aeiou"
+// let output = ""
+
+
+// MARK: - My Solution
+
+/*
+ */
+
+// func removeVowels(_ s: String) -> String {
+//     var arr = Array(s)
+//     let vowels: [Character] = ["a", "e", "i", "o", "u"]
+//
+//     for item in vowels {
+//         arr = arr.filter { $0 != item }
+//     }
+//
+//     return String(arr)
+// }
+
+
+// MARK: - Play
+
+// let result = removeVowels(s) == output
+// print("result :", result)
+
+
+// MARK: - Best practice
--- LeetCode/main.swift
+++ LeetCode/main.swift
@@ -8,29 +8,52 @@
 // MARK: - Description
 
 /*
- 1119. Remove Vowels from a String
+ 1431. Kids With the Greatest Number of Candies
  
- Given a string s, remove the vowels 'a', 'e', 'i', 'o', and 'u' from it, and return the new string.
+ There are n kids with candies. You are given an integer array candies, where each candies[i] represents the number of candies the ith kid has, and an integer extraCandies, denoting the number of extra candies that you have.
+
+ Return a boolean array result of length n, where result[i] is true if, after giving the ith kid all the extraCandies, they will have the greatest number of candies among all the kids, or false otherwise.
+
+ Note that multiple kids can have the greatest number of candies.
  
  Example 1:
- Input: s = "leetcodeisacommunityforcoders"
- Output: "ltcdscmmntyfrcdrs"
+ Input: candies = [2,3,5,1,3], extraCandies = 3
+ Output: [true,true,true,false,true]
+ Explanation: If you give all extraCandies to:
+ - Kid 1, they will have 2 + 3 = 5 candies, which is the greatest among the kids.
+ - Kid 2, they will have 3 + 3 = 6 candies, which is the greatest among the kids.
+ - Kid 3, they will have 5 + 3 = 8 candies, which is the greatest among the kids.
+ - Kid 4, they will have 1 + 3 = 4 candies, which is not the greatest among the kids.
+ - Kid 5, they will have 3 + 3 = 6 candies, which is the greatest among the kids.
  
  Example 2:
- Input: s = "aeiou"
- Output: ""
+ Input: candies = [4,2,1,1,2], extraCandies = 1
+ Output: [true,false,false,false,false]
+ Explanation: There is only 1 extra candy.
+ Kid 1 will always have the greatest number of candies, even if a different kid is given the extra candy.
+ 
+ Example 3:
+ Input: candies = [12,1,12], extraCandies = 10
+ Output: [true,false,true]
  */
 
 
 // MARK: - Inputs
 
 // example 1
-let s = "leetcodeisacommunityforcoders"
-let output = "ltcdscmmntyfrcdrs"
+let candies = [2,3,5,1,3]
+let extraCandies = 3
+let output = [true,true,true,false,true]
 
 // example 2
-// let s = "aeiou"
-// let output = ""
+// let candies = [4,2,1,1,2]
+// let extraCandies = 1
+// let output = [true,false,false,false,false]
+
+// example 3
+// let candies = [12,1,12]
+// let extraCandies = 10
+// let output = [true,false,true]
 
 
 // MARK: - My Solution
@@ -38,22 +61,16 @@ let output = "ltcdscmmntyfrcdrs"
 /*
  */
 
-func removeVowels(_ s: String) -> String {
-    var arr = Array(s)
-    let vowels: [Character] = ["a", "e", "i", "o", "u"]
-    
-    for item in vowels {
-        arr = arr.filter { $0 != item }
-    }
-    
-    return String(arr)
+func kidsWithCandies(_ candies: [Int], _ extraCandies: Int) -> [Bool] {
+    return [false]
 }
 
 
 // MARK: - Play
 
-let result = removeVowels(s) == output
+let result = kidsWithCandies(candies, extraCandies) == output
 print("result :", result)
 
 
 // MARK: - Best practice
+
