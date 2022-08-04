70f45d312511328e78556c20c40a4a457a5a7aec
danielsychoo <sungyeop.choo@gmail.com>
2022-08-04 11:12:29 +0900

    [Update] Solve leetCode 594. Longest Harmonious Subsequence

--- LeetCode/main.swift
+++ LeetCode/main.swift
@@ -53,7 +53,23 @@ let output = 5
  */
 
 func findLHS(_ nums: [Int]) -> Int {
-    return 0
+    var dict: [Int:Int] = [:]
+    nums.forEach {
+        if dict[$0] == nil { dict[$0] = 1 }
+        else { dict[$0]! += 1 }
+    }
+
+    var count = 0
+    var max = 0
+    dict.keys.forEach {
+        if dict[$0 + 1] != nil {
+            count = dict[$0]! + dict[$0 + 1]!
+            
+            if count > max { max = count }
+        }
+    }
+    
+    return max
 }
 
 
