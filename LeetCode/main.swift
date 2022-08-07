e4c0f42bde23288016cf6443498c04b980472848
danielsychoo <sungyeop.choo@gmail.com>
2022-08-08 08:59:19 +0900

    [Update] Solve leetCode 1523. Count Odd Numbers in an Interval Range

--- LeetCode/main.swift
+++ LeetCode/main.swift
@@ -43,7 +43,19 @@ let output = 3
  */
 
 func countOdds(_ low: Int, _ high: Int) -> Int {
-    return 0
+    // var count = 0
+    // for i in low ... high {
+    //     if i % 2 != 0 { result += 1 }
+    // }
+    var num = (low % 2 != 0) ? (low) : (low + 1)
+    var count = 0
+    
+    
+    while num <= high {
+        num += 2
+        count += 1
+    }
+    return count
 }
 
 
