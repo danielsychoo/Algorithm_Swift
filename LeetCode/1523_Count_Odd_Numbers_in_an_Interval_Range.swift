a0cb3627b1dac873e68c948814044057a0cb96ba
danielsychoo <sungyeop.choo@gmail.com>
2022-08-08 09:02:58 +0900

    [Feat] Add leetCode 1805. Number of Different Integers in a String

---
+++ LeetCode/1523_Count_Odd_Numbers_in_an_Interval_Range.swift
@@ -0,0 +1,68 @@
+//
+//  main.swift
+//  Algorithm_Swift
+//
+//  Created by sungyeopTW on 2022/08/08.
+//
+
+// MARK: - Description
+
+/*
+ 1523. Count Odd Numbers in an Interval Range
+
+ Given two non-negative integers low and high. Return the count of odd numbers between low and high (inclusive).
+ 
+ Example 1:
+ Input: low = 3, high = 7
+ Output: 3
+ Explanation: The odd numbers between 3 and 7 are [3,5,7].
+ 
+ Example 2:
+ Input: low = 8, high = 10
+ Output: 1
+ Explanation: The odd numbers between 8 and 10 are [9].
+ */
+
+
+// MARK: - Inputs
+
+// example 1
+// let low = 3
+// let high = 7
+// let output = 3
+
+// example 2
+// let low = 8
+// let high = 10
+// let output = 1
+
+
+// MARK: - My Solution
+
+/*
+ */
+
+// func countOdds(_ low: Int, _ high: Int) -> Int {
+//     // var count = 0
+//     // for i in low ... high {
+//     //     if i % 2 != 0 { result += 1 }
+//     // }
+//     var num = (low % 2 != 0) ? (low) : (low + 1)
+//     var count = 0
+//
+//
+//     while num <= high {
+//         num += 2
+//         count += 1
+//     }
+//     return count
+// }
+
+
+// MARK: - Play
+
+// let result = countOdds(low, high) == output
+// print("result :", result)
+
+
+// MARK: - Best practice
--- LeetCode/main.swift
+++ LeetCode/main.swift
@@ -8,61 +8,66 @@
 // MARK: - Description
 
 /*
- 1523. Count Odd Numbers in an Interval Range
+ 1805. Number of Different Integers in a String
 
- Given two non-negative integers low and high. Return the count of odd numbers between low and high (inclusive).
+ You are given a string word that consists of digits and lowercase English letters.
+
+ You will replace every non-digit character with a space.
+ For example, "a123bc34d8ef34" will become " 123  34 8  34".
+ Notice that you are left with some integers that are separated by at least one space: "123", "34", "8", and "34".
+
+ Return the number of different integers after performing the replacement operations on word.
+
+ Two integers are considered different if their decimal representations without any leading zeros are different.
+
+
  
  Example 1:
- Input: low = 3, high = 7
+ Input: word = "a123bc34d8ef34"
  Output: 3
- Explanation: The odd numbers between 3 and 7 are [3,5,7].
+ Explanation: The three different integers are "123", "34", and "8". Notice that "34" is only counted once.
  
  Example 2:
- Input: low = 8, high = 10
+ Input: word = "leet1234code234"
+ Output: 2
+ 
+ Example 3:
+ Input: word = "a1b01c001"
  Output: 1
- Explanation: The odd numbers between 8 and 10 are [9].
+ Explanation: The three integers "1", "01", and "001" all represent the same integer because
+ the leading zeros are ignored when comparing their decimal values.
  */
 
 
 // MARK: - Inputs
 
 // example 1
-let low = 3
-let high = 7
+let word = "a123bc34d8ef34"
 let output = 3
 
 // example 2
-// let low = 8
-// let high = 10
-// let output = 1
+// let word = "leet1234code234"
+// let output = 2
 
+// example 3
+// let word = "a1b01c001"
+// let output = 1
 
 // MARK: - My Solution
 
 /*
  */
 
-func countOdds(_ low: Int, _ high: Int) -> Int {
-    // var count = 0
-    // for i in low ... high {
-    //     if i % 2 != 0 { result += 1 }
-    // }
-    var num = (low % 2 != 0) ? (low) : (low + 1)
-    var count = 0
-    
-    
-    while num <= high {
-        num += 2
-        count += 1
-    }
-    return count
+func numDifferentIntegers(_ word: String) -> Int {
+    return 0
 }
 
 
 // MARK: - Play
 
-let result = countOdds(low, high) == output
+let result = numDifferentIntegers(word) == output
 print("result :", result)
 
 
 // MARK: - Best practice
+
