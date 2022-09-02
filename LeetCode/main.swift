f9cc75fa1aa24466af57d3edc71daa3745a62cd8
danielsychoo <sungyeop.choo@gmail.com>
2022-09-02 12:25:45 +0900

    [Update] Solve leetCode 2068. Check Whether Two Strings are Almost Equivalent

--- LeetCode/main.swift
+++ LeetCode/main.swift
@@ -13,12 +13,14 @@ import Foundation
  2068. Check Whether Two Strings are Almost Equivalent
  
  Two strings word1 and word2 are considered almost equivalent if the differences between the frequencies of each letter from 'a' to 'z' between word1 and word2 is at most 3.
-
  Given two strings word1 and word2, each of length n, return true if word1 and word2 are almost equivalent, or false otherwise.
-
  The frequency of a letter x is the number of times it occurs in the string.
  
  ---
+ 
+ 두 문자열 word1과 word2는 word1과 word2 사이의 'a'에서 'z'까지의 각 문자의 빈도 차이가 최대 3인 경우 거의 동일한 것으로 간주됩니다.
+ 각각 길이가 n인 두 개의 문자열 word1과 word2가 주어지면 word1과 word2가 거의 같으면 true를 반환하고 그렇지 않으면 false를 반환합니다.
+ 문자 x의 빈도는 문자열에서 나타나는 횟수입니다.
  
  Example 1:
  Input: word1 = "aaaa", word2 = "bccb"
@@ -69,10 +71,24 @@ let output = false
 // MARK: - My Solution
 
 /*
+ 1. word1과 word2의 각 문자를 dictionary로 ?
+ 2. 같은 key의 value 차이가 4 이상이면 false 아니면 true
  */
 
 func checkAlmostEquivalent(_ word1: String, _ word2: String) -> Bool {
-    return false
+    var dict: [Character: Int] = [:]
+    word1.forEach { character in
+        if let _ = dict[character] { dict[character]! += 1 }
+        else { dict[character] = 1 }
+    }
+    
+    word2.forEach { character in
+        if let _ = dict[character] { dict[character]! -= 1 }
+        else { dict[character] = -1 }
+    }
+    
+    if let _ = dict.values.first(where: { abs($0) > 3 }) { return false }
+    else { return true }
 }
 
 
