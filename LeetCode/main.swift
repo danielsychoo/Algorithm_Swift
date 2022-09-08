b9d300836408692490bcb8ba5f4640d338a55c65
danielsychoo <sungyeop.choo@gmail.com>
2022-09-08 10:45:31 +0900

    [Feat] Add & Solve leetCode 617. Merge Two Binary Trees

--- LeetCode/main.swift
+++ LeetCode/main.swift
@@ -62,22 +62,30 @@
 /*
  */
 
-public class TreeNode {
-    public var val: Int
-    public var left: TreeNode?
-    public var right: TreeNode?
-    public init() { self.val = 0; self.left = nil; self.right = nil; }
-    public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
-    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
-        self.val = val
-        self.left = left
-        self.right = right
-    }
-}
+// public class TreeNode {
+//     public var val: Int
+//     public var left: TreeNode?
+//     public var right: TreeNode?
+//     public init() { self.val = 0; self.left = nil; self.right = nil; }
+//     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+//     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+//         self.val = val
+//         self.left = left
+//         self.right = right
+//     }
+// }
 
-func mergeTrees(_ root1: TreeNode?, _ root2: TreeNode?) -> TreeNode? {
-    
-}
+// func mergeTrees(_ root1: TreeNode?, _ root2: TreeNode?) -> TreeNode? {
+//     guard let tree1 = root1, let tree2 = root2 else {
+//                return root1 == nil ? root2 : root1
+//           }
+//
+//     let result = TreeNode((tree1.val) + (tree2.val))
+//     result.left = mergeTrees(tree1.left, tree2.left)
+//     result.right = mergeTrees(tree1.right, tree2.right)
+//
+//     return result
+// }
 
 
 // MARK: - Play
