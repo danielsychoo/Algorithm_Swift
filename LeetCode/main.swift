e1a0aab3d9003f97bf38d347efa453471235f81a
danielsychoo <sungyeop.choo@gmail.com>
2022-09-08 11:59:32 +0900

    [Update] Solve leetCode 104. Maximum Depth of Binary Tree

--- LeetCode/main.swift
+++ LeetCode/main.swift
@@ -58,9 +58,11 @@
  * }
  */
 
-func maxDepth(_ root: TreeNode?) -> Int {
-
-}
+// func maxDepth(_ root: TreeNode?) -> Int {
+//     guard let root = root else { return 0 }
+//
+//     return max(maxDepth(root.left), maxDepth(root.right)) + 1
+// }
 
 
 // MARK: - Play
