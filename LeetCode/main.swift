4810b51e9f5ff80a400f2cd0fc816aa2ad009bc1
danielsychoo <sungyeop.choo@gmail.com>
2022-09-08 15:14:15 +0900

    [Update] Solve leetCode 2363. Merge Similar Items

--- LeetCode/main.swift
+++ LeetCode/main.swift
@@ -12,13 +12,23 @@
  
  You are given two 2D integer arrays, items1 and items2, representing two sets of items. Each array items has the following properties:
 
- items[i] = [valuei, weighti] where valuei represents the value and weighti represents the weight of the ith item.
- The value of each item in items is unique.
+ - items[i] = [valuei, weighti] where valuei represents the value and weighti represents the weight of the ith item.
+ - The value of each item in items is unique.
+ 
  Return a 2D integer array ret where ret[i] = [valuei, weighti], with weighti being the sum of weights of all items with value valuei.
 
  Note: ret should be returned in ascending order by value.
  
  ---
+ 
+ 두 개의 항목 집합을 나타내는 두 개의 2D 정수 배열 items1 및 items2가 제공됩니다. 각 배열 항목에는 다음과 같은 속성이 있습니다.
+ 
+ - items[i] = [valuei, weighti] 여기서 valuei는 값을 나타내고 weighti는 i번째 항목의 가중치를 나타냅니다.
+ - 항목에 있는 각 항목의 값은 고유합니다.
+ 
+ ret[i] = [valuei, weighti]인 2D 정수 배열 ret를 반환합니다. 여기서 weighti는 valuei 값을 가진 모든 항목의 가중치 합입니다.
+ 
+ 참고: ret는 값에 따라 오름차순으로 반환되어야 합니다.
  
  Example 1:
  Input: items1 = [[1,1],[4,5],[3,8]], items2 = [[3,1],[1,5]]
@@ -53,9 +63,9 @@
 // MARK: - Inputs
 
 // example 1
-let items1 = [[1,1],[4,5],[3,8]]
-let items2 = [[3,1],[1,5]]
-let output = [[1,6],[3,9],[4,5]]
+// let items1 = [[1,1],[4,5],[3,8]]
+// let items2 = [[3,1],[1,5]]
+// let output = [[1,6],[3,9],[4,5]]
 
 // example 2
 // let items1 = [[1,1],[3,2],[2,3]]
@@ -63,9 +73,9 @@ let output = [[1,6],[3,9],[4,5]]
 // let output = [[1,4],[2,4],[3,4]]
 
 // example 3
-// let items1 = [[1,3],[2,2]]
-// let items2 = [[7,1],[2,2],[1,4]]
-// let output = [[1,7],[2,4],[7,1]]
+let items1 = [[1,3],[2,2]]
+let items2 = [[7,1],[2,2],[1,4]]
+let output = [[1,7],[2,4],[7,1]]
 
 
 // MARK: - My Solution
@@ -74,8 +84,39 @@ let output = [[1,6],[3,9],[4,5]]
  */
 
 func mergeSimilarItems(_ items1: [[Int]], _ items2: [[Int]]) -> [[Int]] {
- return [[0]]
+    var dict: [Int:Int] = [:]
+
+    items1.forEach { item in
+        if dict[item[0]] != nil { dict[item[0]]! += item[1] }
+        else { dict[item[0]] = item[1] }
+    }
+
+    items2.forEach { item in
+        if dict[item[0]] != nil { dict[item[0]]! += item[1] }
+        else { dict[item[0]] = item[1] }
+    }
+
+    return dict.keys.sorted().map { [$0, dict[$0]!] }
 }
+
+// func mergeSimilarItems(_ items1: [[Int]], _ items2: [[Int]]) -> [[Int]] {
+//     var dict: [Int:Int] = [:]
+//
+//     items1.forEach { item in
+//         let key = item[0], value = item[1]
+//         if dict[key] != nil { dict[key]! += value }
+//         else { dict[key] = value }
+//     }
+//
+//     items2.forEach { item in
+//         let key = item[0], value = item[1]
+//         if dict[key] != nil { dict[key]! += value }
+//         else { dict[key] = value }
+//     }
+//
+//     let result = dict.keys.sorted().map { [$0, dict[$0]!] }
+//     return result
+// }
 
 
 // MARK: - Play
