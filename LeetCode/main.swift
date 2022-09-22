6e3bef500ddbef6d8b6c2f7f2c52909c189acddc
danielsychoo <sungyeop.choo@gmail.com>
2022-09-22 12:22:30 +0900

    [Update] Solve leetCode 496. Next Greater Element I

--- LeetCode/main.swift
+++ LeetCode/main.swift
@@ -58,7 +58,16 @@ let output = [-1,3,-1]
  */
 
 func nextGreaterElement(_ nums1: [Int], _ nums2: [Int]) -> [Int] {
-    return []
+    return nums1.map {
+        var index = nums2.firstIndex(of: $0)! + 1
+        
+        while index < nums2.count {
+            if nums2[index] > $0 { return nums2[index] }
+            else { index += 1 }
+        }
+        
+        return -1
+    }
 }
 
 
