767dcfd518e192228020b6eabbfd39f4ab06fc16
danielsychoo <sungyeop.choo@gmail.com>
2022-10-04 18:30:31 +0900

    [Update] Solve leetCode 925. Long Pressed Name

--- LeetCode/main.swift
+++ LeetCode/main.swift
@@ -5,6 +5,8 @@
 //  Created by sungyeopTW on 2022/09/23.
 //
 
+import Foundation
+
 // MARK: - Description
 
 /*
@@ -34,15 +36,39 @@
 // MARK: - Inputs
 
 // example 1
-let name = "alex"
-let typed = "aaleex"
-let output = true
+// let name = "alex"
+// let typed = "aaleex"
+// let output = true
 
 // example 2
 // let name = "saeed"
 // let typed = "ssaaedd"
 // let output = false
 
+// exmaple 3
+// let name = "leelee"
+// let typed = "lleeelee"
+// let output = true
+
+// example 4
+// let name = "vtkgn"
+// let typed = "vttkgnn"
+// let output = true
+
+// example 5
+// let name = "alex"
+// let typed = "aaleexeex"
+// let output = false
+
+// example 6
+// let name = "alexd"
+// let typed = "ale"
+// let output = false
+
+// example 7
+let name = "a"
+let typed = "aaaaaaaaaaaaaaaaa"
+let output = true
 
 
 // MARK: - My Solution
@@ -51,8 +77,41 @@ let output = true
  */
 
 func isLongPressedName(_ name: String, _ typed: String) -> Bool {
-    return false
+    
+    // edgeCase
+    if name.first != typed.first {
+        return false
+    }
+    if Set(name).count == 1 && Set(typed).count == 1 && name.first == typed.first { return true }
+    
+    var m = 1
+    var over = 0
+    for i in 1..<typed.count {
+        let currentTyped = getCharacter(typed, with: i)
+        let currentName = getCharacter(name, with: m)
+        let beforeName = getCharacter(name, with: m-1)
+        
+        if currentTyped == currentName { m += 1 }
+        else if currentTyped == beforeName {
+            over += 1
+            continue
+        }
+        else { return false }
+    }
+    
+    if m > name.count { return (m - over) <= name.count }
+    else { return m == name.count }
 }
+
+func getCharacter(_ string: String, with i: Int) -> Character {
+    if i > string.count-1 {
+        return string.last!
+    } else {
+        let index = string.index(string.startIndex, offsetBy: i)
+        return string[index]
+    }
+}
+
 
 // MARK: - Play
 
